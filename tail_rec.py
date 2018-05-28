990459b0ea674299f554c51004a4f21036ac369f
gucliti <liutiewh@gmail.com>
2018-05-28 23:05:36 +0200

    Update tail_rec.py

--- tail_rec.py
+++ tail_rec.py
@@ -6,7 +6,9 @@ def tramp(gen, *args, **kwargs):
     """Copyright, 2012, Alex Beal"""
     g = gen(*args, **kwargs)
     while isinstance(g, types.GeneratorType):
+        print('+++ before: ', g)
         g = next(g)
+        print('--- After  : ', g)
     return g
 
 
@@ -21,8 +23,26 @@ def s(n, acc=0):
     if n == 0:
         yield acc
     else:
-        yield s(n -1, acc + n)
+        yield s(n - 1, acc + n)
 
 
-print(tramp(f, 1))
-print(tramp(s, 100))
+print(tramp(f, 2))
+print(tramp(s, 2))
+
+#foo.py
+#+++ before : <generator object f at 0x03A09120>
+#--- after  : <generator object f at 0x03A5F660>
+#+++ before : <generator object f at 0x03A5F660>
+#--- after  : <generator object f at 0x03A09120>
+#+++ before : <generator object f at 0x03A09120>
+#--- after  : 1
+#1
+#+++ before : <generator object s at 0x03A09120>
+#--- after  : <generator object s at 0x03A5F660>
+#+++ before : <generator object s at 0x03A5F660>
+#--- after  : <generator object s at 0x03A09120>
+#+++ before : <generator object s at 0x03A09120>
+#--- after  : 3
+#3
+
+Process finished with exit code 0
