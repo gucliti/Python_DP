67d3095506be0c43626d978e069be9fad7f33911
gucliti <liutiewh@gmail.com>
2019-02-26 22:35:27 +0100

    Create Play_something.py

---
+++ Play_something.py
@@ -0,0 +1,65 @@
+#https://techdevguide.withgoogle.com/paths/advanced/compress-decompression#!
+The Challenge
+#In this exercise, you're going to decompress a compressed string.
+
+#Your input is a compressed string of the format number[string] and the decompressed output form should be the string written number times. For example:
+
+#The input
+
+#3[abc]4[ab]c
+
+#Would be output as
+
+#abcabcabcababababc
+
+#Other rules
+#Number can have more than one digit. For example, 10[a] is allowed, and just means aaaaaaaaaa
+
+#One repetition can occur inside another. For example, 2[3[a]b] decompresses into aaabaaab
+
+#Characters allowed as input include digits, small English letters and brackets [ ].
+
+#Digits are only to represent amount of repetitions.
+
+#Letters are just letters.
+
+#Brackets are only part of syntax of writing repeated substring.
+
+#Input is always valid, so no need to check its validity.
+
+#Learning objectives
+#This question gives you the chance to practice with strings, recursion, algorithm, compilers, automata, and loops. It’s also an opportunity to work on coding with better efficiency.
+
+
+def split_num(a_str):
+    """Split string like "ab2", "a" or "23"
+    into ('ab', 2), ("a", 1) or ("", 23)"""
+    idx = None
+    for i in iter(a_str):
+        if i.isdigit():
+            idx = a_str.index(i)
+            break
+    if idx == None:
+        return (a_str[:idx], int('1'))
+    else:
+        return (a_str[:idx], int(a_str[idx:]))
+
+def parse_str(s):
+    print(f'received : {s}')
+    buf = ''
+    stack = []
+    for i in range(len(s)):
+        if s[i] == ']':
+            _tmp = stack.pop()
+            _h, _t = split_num(_tmp)
+            buf = _h + buf * _t
+        elif s[i] == '[':
+            stack.append(buf)
+            buf = ''
+        else:
+            buf += s[i]
+    return buf
+
+s = 'a2[b3[c]d]edfa2[ds]'
+g = parse_str(s)
+g
